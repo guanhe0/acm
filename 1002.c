cf6a70b52aec5d0d65ca6b02d9d2968a58f00711
guanhe <307784732@qq.com>
2016-10-20 18:21:43 +0800

    add 1002.c

--- 1002.c
+++ 1002.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-int add(char a[],char b[]);
+void add(char a[],char b[]);
 int main()
 {
 	int T;
@@ -15,27 +15,86 @@ int main()
 		scanf("%s",a);
 		scanf("%s",b);
 
-		#printf("%s\n%s\n",a,b);
+		//printf("%s\n%s\n",a,b);
 		printf("Case %d:\n",index++);
-		sum=add(a,b);
-		printf("%s + %s = %d\n\n",a,b,sum);
+		
+		printf("%s + %s = ",a,b);
+		add(a,b);
 	}
 	return 0;
 }
-int add(char a[],char b[])
+void add(char a[],char b[])
 {
 	char c[1002];
 	memset(c,0,sizeof(c));
-	int len-a=strlen(a);
-	int len-b=strlen(b);
+	int len_a=strlen(a);
+	int len_b=strlen(b);
+//	printf("len_a=%d len_b=%d",len_a,len_b);
 	int arra=0;
 	int index=0;
-	while(len-a&&len-b)
+	while(len_a&&len_b)
 	{
-		c[index]='0'+a[len-a-1]-'0'+b[len-b-1]-'0';
-		if(c[index]>'1)
-		len-a--;
-		len-b--;
+		c[index]='0'+a[len_a-1]-'0'+b[len_b-1]-'0'+arra;
+		if((c[index]-'0')>=10)
+		{
+			arra=1;
+			c[index]='0'+(c[index]-'0')%10;	
+		}
+		else
+		{
+			arra=0;
+		}
+		//printf("c[]=%d arra=%d\n",c[index]-'0',arra);
+		len_a--;
+		len_b--;
+		index++;
 	}
-	return 0;
+	if(len_a)
+	{   int i;
+		for(i=len_a-1;i>=0;i--)
+		{
+			c[index]='0'+a[i]-'0'+arra;
+			if((c[index]-'0')>=10)
+			{
+			   arra=1;
+			   c[index]='0'+(c[index]-'0')%10;	   
+			}
+			else
+			{
+				arra=0;
+			}
+			index++;
+		}
+	}
+	if(len_b)
+	{   int j;
+		for(j=len_b-1;j>=0;j--)
+		{
+			c[index]='0'+b[j]-'0'+arra;
+			if((c[index]-'0')>=10)
+			{
+				arra=1;
+				c[index]='0'+(c[index]-'0')%10;
+			}
+			else
+			{
+				arra=0;
+			}
+			index++;
+		}
+	}
+	if(arra)
+	{
+		c[index++]='0'+arra;
+	}
+	char tmp;
+	int k;
+	for(k=index-1;k>=0;k--)
+	{
+	//	tmp=c[k-1];
+	//	c[k-1]=c[index-k];
+	//	c[index-k]=tmp;
+	    printf("%c",c[k]);
+	}
+	printf("\n\n");
 }
