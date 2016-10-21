dce933eca946fc3486d5d1f6f58e7de6dcd7a138
guanhe <307784732@qq.com>
2016-10-21 17:40:46 +0800

    add 1003.c

---
+++ 1003.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <malloc.h>
+
+int main()
+{
+	int T;
+	scanf("%d",&T);
+	while(T--)
+	{
+		int N;
+		scanf("%d",&N);
+		int *arra=(int *)malloc(N*sizeof(int));
+		int i=0;
+		while(N--)
+		{
+			scanf("%d",&arra[i++]);
+		}
+		int j;
+		for(j = 0; j < i; j++)
+		{
+			
+		}
+	}
+	return 0;
+}
