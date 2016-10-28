740af4553dde3ee9f544dec2e0433b79497a684c
guanhe <307784732@qq.com>
2016-10-28 10:48:01 +0800

    find the raw of an

--- 1005a.c
+++ 1005a.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#define MAX_RAND 7
 int main()
 {
 	int A,B,n;
@@ -13,22 +13,24 @@ int main()
 		else
 		{
 			int fn_1=1,fn_2=1;
-			int i=3;
-			int fn=0;
+			int i=2;
+			int fn[MAX_RAND-1];
+			fn[0]=fn_1;
+			fn[1]=fn_2;
 			if(n==1|n==2)
 			{
 				printf("%d\n",1);
 			}
 			else
 			{
-				while(i<=n)
+				while(i<MAX_RAND)
 				{
-					fn=(A*fn_1+B*fn_2)%7;
+					fn[i]=(A*fn_1+B*fn_2)%MAX_RAND;
 					fn_2=fn_1;
-					fn_1=fn;
+					fn_1=fn[i];
 					i++;
 				}
-				printf("%d\n",fn);
+				printf("%d\n",fn[(n%(MAX_RAND-1))-1]);
 			}
 		}
 	}
